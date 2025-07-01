3372885b02a73f96e0ea44377bc725d6eaf951e0
ishanDthapa <ishantthapa895@gmail.com>
2025-07-01 17:07:04 +0530

    Hello class Updated

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Git Session");
+        System.out.println("Git Session");
+        System.out.println("Git Session");
+        System.out.println("Git Session");
+        System.out.println("Git Session");
+        System.out.println("Git Session");
+
     }
 }
